abbde53f11d3dfc75e6a40d91110cd07e0498158
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-04-22 23:39:46 +0300

    fix.

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -2,9 +2,9 @@ export const settings = {
 
     JWT_SECRET_AccessTOKEN: process.env.JWT_AccessTOKEN_SECRET || 'accessToken_secret' ,
 
-    TIME_LIFE_AccessTOKEN: '600s',
+    TIME_LIFE_AccessTOKEN: '600000s',
 
     JWT_SECRET_RefreshTOKEN: process.env.JWT_RefreshTOKEN_SECRET || 'refreshToken' ,
 
-    TIME_LIFE_RefreshTOKEN: '1200s',
+    TIME_LIFE_RefreshTOKEN: '1200000s',
 }
